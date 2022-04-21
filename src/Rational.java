87d0411daae55ab3abf504b9bbcfa2c683f16de6
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-04-21 19:45:07 +0300

    adding helper methods

--- src/Rational.java
+++ src/Rational.java
@@ -1,8 +1,22 @@
 public class Rational {
-    private int a;
-    private int b;
-    Rational( int numerator , int denominator){
-        this.a = numerator;
-        this.b = denominator;
+    private int numerator;
+    private int denominator;
+    public Rational( int numerator , int denominator){
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+
+    // default constructor
+    public Rational(){
+        numerator =1;
+        denominator =1;
+    }
+
+    //Reducing the fraction to is lowest terms
+    private void reduce(){
+        int divisor = gcf(numerator,denominator);
+        numerator= numerator/divisor;
+        denominator= denominator/divisor;
     }
 }
